3c1fea44c6bae514e9471e4af86619c80567e43d
Aleksei Kostrykin <me@ikao.dev>
2023-10-31 18:18:10 +0300

    feat: первичная имплементация расчета/перерасчета уровня на оснвове калькуляции

---
+++ Assets/Plugins/CREEXTEAM/UniXP/Infrastructure/EvaluatedLevelRecalculation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UniXP.Domain;
+using UniXP.Domain.Model;
+
+namespace UniXP.Infrastructure
+{
+    public class EvaluatedLevelRecalculation : ILevelRecalculation
+    {
+        private ILevelEvaluator _evaluator;
+        public EvaluatedLevelRecalculation(ILevelEvaluator evaluator)
+        {
+            _evaluator = evaluator;
+        }
+        
+        public void FromXP(ReactiveUnsignedInteger xp, ReactiveUnsignedInteger level)
+        {
+            if (xp == null)
+                throw new NullReferenceException();
+            
+            if (level == null)
+                throw new NullReferenceException();
+            
+            var calcLevel = _evaluator.XPToLevel(xp.Value);
+            level.Set(calcLevel);
+        }
+
+        public void ToXP(ReactiveUnsignedInteger xp, ReactiveUnsignedInteger level)
+        {
+            if (xp == null)
+                throw new NullReferenceException();
+            
+            if (level == null)
+                throw new NullReferenceException();
+            
+            var calcXP = _evaluator.XPToLevel(xp.Value);
+            xp.Set(calcXP);
+        }
+    }
+}
---
+++ Assets/Plugins/CREEXTEAM/UniXP/Infrastructure/LinearLevelEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniXP.Domain;
+
+namespace UniXP.Infrastructure
+{
+    public class LinearLevelEvaluator : ILevelEvaluator
+    {
+        private readonly uint _minXP;
+        
+        public LinearLevelEvaluator(uint minXP)
+        {
+            _minXP = minXP;
+        }
+        
+        public uint LevelToXP(uint level)
+        {
+            return (uint) ((Math.Pow(level, 2) + level) / 2 * _minXP - (level * _minXP));
+        }
+        
+        public uint XPToLevel(uint xp)
+        {
+            return (uint) (Math.Sqrt(_minXP * ((2 * xp) + 25)) + 50) / 100;
+        }
+    }
+}
