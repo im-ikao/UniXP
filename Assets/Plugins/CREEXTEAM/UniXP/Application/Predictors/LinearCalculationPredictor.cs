05697c96d0bfb707cc4efb7518f396ae497b76c2
Aleksei Kostrykin <me@ikao.dev>
2023-10-31 21:45:00 +0300

    feat: добавлен класс, позволяющий просчитать через инспектор, линейный прогресс уровня для удобного геймдизайна

---
+++ Assets/Plugins/CREEXTEAM/UniXP/Application/Predictors/LinearCalculationPredictor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UniXP.Application.Model;
+using UniXP.Domain;
+using UniXP.Infrastructure;
+
+namespace Plugins.CREEXTEAM.UniXP.Application
+{
+    [Serializable]
+    [CreateAssetMenu]
+    public class LinearCalculationPredictor : ScriptableObject
+    {
+        [SerializeField] private List<ExperiencedLevelDto> _levels = new List<ExperiencedLevelDto>();
+        [SerializeField] private uint _minXp = 0;
+        [SerializeField] private uint _predictCount = 0;
+
+        public void OnValidate()
+        {
+            ILevelEvaluator evaluator = new LinearLevelEvaluator(_minXp);
+            
+            _levels.Clear();
+            for (uint i = 1; i < _predictCount; i++)
+            {
+                _levels.Add(new ExperiencedLevelDto(i, evaluator.LevelToXP(i)));
+            }
+        }
+    }
+}
